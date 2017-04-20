1e9184c05f5a2312de0d023634f8b52a2364e049
Pichoman <picho117@live.com.mx>
2017-04-20 14:41:01 -0500

    funcion mayusculas
    implementamos funcion mayusculas (prueba)

--- mayusculas.c
+++ mayusculas.c
@@ -1,5 +1,14 @@
 void mayusculas(char cad1[], char cad2[])
 {
-
+  int i;
+  int conversion= 'a'-'A';
+  
+  for(i=0; cad2[i]!='\0'; i++)
+  {
+    if (cad2[i] >= 'a' && cad2[i] <=z)
+    {
+      cad2[i]= cad2[i]-conversion;
+    }
+  }
 }
 
